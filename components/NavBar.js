8c600e4e47741239c142b67b794011b9803fc556
bokdol11859 <2019147551@yonsei.ac.kr>
2022-09-22 02:26:58 +0900

    styled jsx

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,29 +1,27 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 const NavBar = () => {
   const router = useRouter();
 
   return (
     <nav>
       <Link href="/">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : null
-          }`}
-        >
-          Home
-        </a>
+        <a className={router.pathname === "/" ? "active" : null}>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/about" ? styles.active : null
-          }`}
-        >
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : null}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: blue;
+        }
+      `}</style>
     </nav>
   );
 };
